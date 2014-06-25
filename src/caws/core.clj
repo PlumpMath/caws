c3dc0ef7bf14fdaf6b9c54867dce32c3d6dd8925
Kiril Savino <kiril@gamechanger.io>
2014-06-25 10:26:19 -0400

    some thinking and an nio library

--- src/caws/core.clj
+++ src/caws/core.clj
@@ -1,10 +1,34 @@
-(ns caws.core)
+(ns caws.core
+  (:require [nio.core :as nio]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; concept...
+;; every view gets an in-port and an out-port
+;; they can be socket ports, in which case you push bytes onto them
+;; or they can be HTTP ports, in which case you push content onto them
+;; you can also use symbols to push other stuff
+;; like: (go (>! out-port {:headers {:content-type "text/html" :status 200}}))
+;;       (go (>! out-port "<html></html">))
+;;       (go (>! out-port :close))
 
 (defn start [router & {:keys [ip port]
                        :or {ip "0.0.0.0" port 8080}}]
   )
+
+(defn route [path in-port out-port]
+  )
+
+(defn routes [mappings]
+
+  (loop [prefixes (keys mappings)]
+    (let [prefix (first prefixes)]
+      (if (.startsWith path prefix)
+        (let [next (mappings prefix)]
+          (if (instance? java.util.Map next)
+            (invoke-view (.substring path (count prefix)) next)
+            (next))))))
+  )
+
+(defn bing [in-port out-port]
+  )
+
+(start (route {"/foo" {"/bing" bing}}))
