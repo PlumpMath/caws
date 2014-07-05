56f4e69cd64d7e1d6d042ce6b9e8a1bbd148c828
Kiril Savino <kiril@gamechanger.io>
2014-07-05 14:12:53 -0400

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kiril/caws "0.1.5"
+(defproject org.clojars.kiril/caws "0.1.6"
   :description "a core.async web server, just... caws..."
   :url "http://github.com/kiril/caws"
   :license {:name "MIT License"
